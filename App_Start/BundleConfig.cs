a20d60ccd12bd2d43991dc1888d94e8042e25f85
Kasia Humienna <kasiennahumienna@gmail.com>
2015-12-11 00:41:16 +0100

    merge2

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -8,17 +8,11 @@ namespace TravelTime
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/app").Include(
-                        "~/Content/lib/angular/angular.js",
-                        "~/Content/lib/angular-google-maps/angular-google-maps.js"
-                        ));
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/cordova-android").Include(
-            "~/cordova/cordova.js",
-            "~/cordova/cordova-plugins.js"
-            ));
-
-
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
 
             bundles.Add(new ScriptBundle("~/bundles/angular").Include(
                         "~/Content/lib/underscore.js",
